c8dcc8762ae50bde85f2e541fb32e643ca323863
ing <inyoman_gurnitha@yahoo.com>
2020-07-21 08:07:17 +0700

    3. Using admin interface

--- news/models.py
+++ news/models.py
@@ -6,12 +6,16 @@ class News(models.Model):
 	created_on = models.DateTimeField(auto_now_add=True)
 	via = models.URLField(blank=True)
 
+	def __str__(self):
+		return self.text[:50]
+
 	def total_likes(self):
 		return self.like_set.count()
 
 	class Meta:
 		verbose_name = 'news'
 		verbose_name_plural = 'news'	
+
 
 class Like(models.Model):
 	news = models.ForeignKey(News, on_delete=models.CASCADE)
