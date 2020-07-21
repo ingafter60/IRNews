0419be9f1b3f2c4b148779246e87bee5cae70ac1
ing <inyoman_gurnitha@yahoo.com>
2020-07-21 07:56:57 +0700

    2. Created models for News and Like

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from news.models import News
 
 # Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+	pass
+	# list_display = ('text', 'created_on', 'total_likes')
+	# list_filter = ['created_on']
+	# search_fields = ['text']
+
+admin.site.register(News, NewsAdmin)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+	text = models.TextField()
+	created_on = models.DateTimeField(auto_now_add=True)
+	via = models.URLField(blank=True)
+
+	def total_likes(self):
+		return self.like_set.count()
+
+	class Meta:
+		verbose_name = 'news'
+		verbose_name_plural = 'news'	
+
+class Like(models.Model):
+	news = models.ForeignKey(News, on_delete=models.CASCADE)
+
