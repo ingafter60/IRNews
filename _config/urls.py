cee916d84f56825824cd4972fea81d294832cff1
ing <inyoman_gurnitha@yahoo.com>
2020-07-22 10:53:58 +0700

    8. 3.1 The User’s Model

--- _config/urls.py
+++ _config/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import url
-
+# from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 from news import views
 
 urlpatterns = [
@@ -24,5 +25,9 @@ urlpatterns = [
     # url(r'^$', views.home, name="homepage"),
     url(r'^$', views.IndexView.as_view(), name="homepage"),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # url(r'^login/$', auth_views.login, name="login"),
+    url(r'^login/$', LoginView.as_view(), name="login"),
+    # url(r'^logout/$', auth_views.logout, name="logout"),
+    url(r'^logout/$', LogoutView.as_view(), name="logout"),
 
 ]
